c5a5babbe8938c3b6c17ab78d3791e11ed85358a
竹内俊貴 <cs11054@s.inf.shizuoka.ac.jp>
2014-10-21 22:31:44 +0900

    登録されていないユーザーがログインできる問題を修正、Gitでミスったので再コミット

--- mods/app/models/DBSupport.scala
+++ mods/app/models/DBSupport.scala
@@ -1,8 +1,7 @@
 package models
 
 import scala.slick.driver.H2Driver.simple._
-import Database.ThreadLocal
-import scala.slick.driver.PostgresDriver.simple._
+import Database.threadLocalSession
 
 import play.Play
 
@@ -17,17 +16,12 @@ trait DBSupport {
   val USER = if (DB_PATH.contains("localhost")) "sa" else "zcgmsoybywazlx"
   val PASS = if (DB_PATH.contains("localhost")) null else "HQt5HBNiYxgDE4LdQ337Ry_urb"
 
-  val DB = if (DB_PATH.contains("localhost")) scala.slick.driver.H2Driver.simple.Database
-  else scala.slick.driver.PostgresDriver.simple.Database
-
-  println(DRIVER, USER, PASS)
-
   // ソートの逆順用
   def Desc[T: Ordering] = implicitly[Ordering[T]].reverse
 
   // DBへの接続を補助
   def connectDB[T](f: => T): T = {
-    DB.forURL(DB_PATH, driver = DRIVER, user = USER, password = PASS) withSession {
+    Database.forURL(DB_PATH, driver = DRIVER, user = USER, password = PASS) withSession {
       f
     }
   }
