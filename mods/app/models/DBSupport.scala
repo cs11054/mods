a73a71da2a866f4153044848aec0b18e58bb3a7b
竹内俊貴 <cs11054@s.inf.shizuoka.ac.jp>
2014-10-21 21:36:59 +0900

    迷走中

--- mods/app/models/DBSupport.scala
+++ mods/app/models/DBSupport.scala
@@ -2,18 +2,29 @@ package models
 
 import scala.slick.driver.H2Driver.simple._
 import Database.threadLocalSession
+import play.Play
 
 trait DBSupport {
 
+  val Heroku = false//true // or false
+
+  val DB_PATH = if (!Heroku) "jdbc:h2:tcp://localhost:9093/db"
+  else "postgres://zcgmsoybywazlx:HQt5HBNiYxgDE4LdQ337Ry_urb@ec2-54-83-204-78.compute-1.amazonaws.com:5432/dcn5isfl4oitng"
+    
+  val DRIVER = if (DB_PATH.contains("localhost")) "org.h2.Driver" else "org.postgresql.Driver"
+  val USER = if (DB_PATH.contains("localhost")) "sa" else "zcgmsoybywazlx"
+  val PASS = if (DB_PATH.contains("localhost")) "" else "HQt5HBNiYxgDE4LdQ337Ry_urb"
+
+    println(DRIVER,USER,PASS)
+    
   // ソートの逆順用
   def Desc[T: Ordering] = implicitly[Ordering[T]].reverse
 
   // DBへの接続を補助
   def connectDB[T](f: => T): T = {
-    Database.forURL("jdbc:h2:tcp://localhost:9093/db", driver = "org.h2.Driver", user = "sa") withSession {
+    Database.forURL(DB_PATH, driver = DRIVER, user = USER, password = PASS) withSession {
       f
     }
   }
-
 
 }
