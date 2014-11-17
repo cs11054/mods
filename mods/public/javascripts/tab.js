502e283056c45e9fa9c04c83296eb0a5a0971fa3
竹内俊貴 <cs11054@s.inf.shizuoka.ac.jp>
2014-11-17 14:31:05 +0900

    選択中の課題の色を変更

--- mods/public/javascripts/tab.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-	$('#tab li').click(function() {
-		$('#tab li').removeClass('active');
-		$(this).addClass('active');
-		$('.tabbox').hide();
-		$($(this).find('a').attr('href')).fadeIn();
-		return false;
-	});
-});
